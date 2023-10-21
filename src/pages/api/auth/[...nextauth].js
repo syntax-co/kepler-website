a7107baf1e4e5c58922e647a83a2522a0f0a4cd1
syntax-co <51330734+syntax-co@users.noreply.github.com>
2023-10-21 12:53:47 -0500

    api-data-removed pt3

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,8 +1,42 @@
 import NextAuth, {NextAuthOptions} from 'next-auth';
-
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { PrismaClient } from '@prisma/client';
+const bcrypt = require('bcryptjs');
 
 const authOptions = {
-    
+    secret: process.env.AUTH_SECRET,
+    providers: [
+        CredentialsProvider({
+          // The name to display on the sign in form (e.g. "Sign in with...")
+          name: "Credentials",
+          // `credentials` is used to generate a form on the sign in page.
+          // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+          // e.g. domain, username, password, 2FA token, etc.
+          // You can pass any HTML attribute to the <input> tag through the object.
+          credentials: {
+            username: { label: "Username", type: "text", placeholder: "jsmith" },
+            password: { label: "Password", type: "password" }
+          },
+          async authorize(credentials, req) {
+            const user = {
+                username:'',
+                data:{
+                }
+            }
+            return(user);
+          }
+        })
+      ],
+      callbacks: {
+        async jwt({ token, user }) {
+            user && (token.user = user);
+            return token;
+        },
+        async session({ session, token }) {
+            session.user = token.user;
+            return session;
+        },
+    },
 }
 
 
